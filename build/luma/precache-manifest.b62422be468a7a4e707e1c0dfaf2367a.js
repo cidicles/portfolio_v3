69cfd742cf6bd2737cb8cb1461b097cbbba1e82b
cidi <admin@ericjseaton.com>
2018-12-12 23:04:35 -0800

    luma

---
+++ build/luma/precache-manifest.b62422be468a7a4e707e1c0dfaf2367a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34f40b3dbe54f7a8c0c3f012b2940b1",
+    "url": "/static/media/leixo-demo-webfont.a34f40b3.woff2"
+  },
+  {
+    "revision": "1f304666b15826b15938942e5dc1afd7",
+    "url": "/static/media/leixo-demo-webfont.1f304666.woff"
+  },
+  {
+    "revision": "e5627dd209538a6883d79180655808b8",
+    "url": "/static/media/disolve_regular-webfont.e5627dd2.woff2"
+  },
+  {
+    "revision": "0e6a8d37342c25d0628ec0cdb0fe85ba",
+    "url": "/static/media/disolve_regular-webfont.0e6a8d37.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4442f7bd7709a22312d3",
+    "url": "/static/js/main.4442f7bd.chunk.js"
+  },
+  {
+    "revision": "e41e3f7bbd50a2aa5e37",
+    "url": "/static/js/1.e41e3f7b.chunk.js"
+  },
+  {
+    "revision": "4442f7bd7709a22312d3",
+    "url": "/static/css/main.7bd8f9af.chunk.css"
+  },
+  {
+    "revision": "745ce6b204f704573bcb9c821959b709",
+    "url": "/index.html"
+  }
+];
